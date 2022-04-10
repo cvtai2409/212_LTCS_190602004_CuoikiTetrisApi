6cc8b78a201d76a75f3e63813f171da0080ea961
cvtai2409 <71303956+cvtai2409@users.noreply.github.com>
2022-04-10 15:03:08 +0700

    Update ScoreModel.cs

--- Tetrisgame/Model/ScoreModel.cs
+++ Tetrisgame/Model/ScoreModel.cs
@@ -10,7 +10,7 @@ namespace Tetrisgame.Model
     {
         public string NickName { get; set; }
         public int Score { get; set; }
-        
+        public DateTime dateTime { get; set; }
         public ScoreModel()
         {
 
